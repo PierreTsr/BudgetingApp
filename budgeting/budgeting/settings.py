d6b8e9179435f51f2950ec68ddc7cdd42bf0db2b
Pierre Tessier <pierre.borie.tessier@gmail.com>
2024-01-31 21:07:54 -0800

    fix db file path

--- budgeting/budgeting/settings.py
+++ budgeting/budgeting/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "OPTIONS": {
-            "read_default_file": "../my.cnf",
+            "read_default_file": str(BASE_DIR / "my.cnf"),
         },
     }
 }
