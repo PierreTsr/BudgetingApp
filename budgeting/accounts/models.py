295e6c781baa98c4e2b86bd83a0de92dcb47bb1c
Pierre Tessier <pierre.borie.tessier@gmail.com>
2024-02-06 22:05:56 -0800

    first draft of the data model

--- budgeting/accounts/models.py
+++ budgeting/accounts/models.py
@@ -1,3 +1,36 @@
+from currencies.models import Currency
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Account(models.Model):
+    user = models.ForeignKey(
+        User,
+        models.CASCADE,
+        verbose_name="Owner of the account.",
+    )
+    currency = models.ForeignKey(
+        Currency,
+        models.PROTECT,
+        verbose_name="Currency of the account.",
+        blank=True,
+    )
+    name = models.CharField(
+        max_length=128,
+        verbose_name="Name of the account.",
+    )
+
+
+class Transaction(models.Model):
+    account = models.ForeignKey(
+        Account,
+        models.CASCADE,
+        verbose_name="The account on which this transaction was operated.",
+    )
+    category = models.ForeignKey(
+        "categories.Category",
+        models.RESTRICT,
+    )
+    name = models.CharField(max_length=256)
+    value = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
