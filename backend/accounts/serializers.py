7392a7e430fd69685cf6afd33c36960b029b5fc2
Pierre Tessier <pierre.borie.tessier@gmail.com>
2024-02-16 19:18:00 -0800

    add rest_framework support

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,9 +1,33 @@
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import serializers
+from users.models import User
 
-from .models import Account
+from .models import Account, Transaction
 
 
 class AccountSerializer(serializers.ModelSerializer):
+    account_type = serializers.CharField(source="get_account_type_display")
+    balance = serializers.ReadOnlyField(source="get_balance")
+
     class Meta:
         model = Account
-        fields = ("id", "name", "currency")
+        fields = ("id", "user", "name", "currency", "account_type", "balance")
+        read_only_fields = ("user", "balance")
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = "__all__"
+
+
+class OfxSerializer(serializers.Serializer):
+    content = serializers.CharField()
+    user = serializers.IntegerField()
+
+    def validate_user(self, value):
+        try:
+            user = User.objects.get(pk=int(value))
+            return user
+        except ObjectDoesNotExist:
+            raise serializers.ValidationError("The provided user does not exists")
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,29 +1,48 @@
-from django.views import generic
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.filters import OrderingFilter
+from rest_framework.response import Response
 
 from .models import Account, Transaction
-from .serializers import AccountSerializer
-
-
-class IndexView(generic.ListView):
-    template_name = "accounts/index.html"
-
-
-# class AccountView(generic.DetailView):
-#     model = Account
-
-#     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-#         context = super(AccountView, self).get_context_data(**kwargs)
-#         return {
-#             **context["account"].transaction_set.all().aggregate(balance=Sum("value")),
-#             **context,
-#         }
+from .serializers import AccountSerializer, OfxSerializer, TransactionSerializer
 
 
 class AccountView(viewsets.ModelViewSet):
     serializer_class = AccountSerializer
     queryset = Account.objects.all()
 
+    @action(detail=False, methods=["post"])
+    def create_from_ofx(self, request) -> Response:
+        serializer = OfxSerializer(data=request.data)
+        if serializer.is_valid():
+            content = serializer.validated_data["content"]
+            user = serializer.validated_data["user"]
+            account = Account.create_from_file(user, "ofx", content)
+            return Response(AccountSerializer(account).data)
+        else:
+            return Response(serializer.errors)
 
-class TransactionView(generic.DetailView):
-    model = Transaction
+    @action(detail=True, methods=["post"])
+    def update_from_ofx(self, request, pk=None) -> Response:
+        account = self.get_object()
+        serializer = OfxSerializer(data=request.data)
+        if serializer.is_valid():
+            if serializer.validated_data["user"].id == account.user.id:
+                content = serializer.validated_data["content"]
+                transactions = account.update_from_file("ofx", content)
+                return Response(TransactionSerializer(transactions, many=True).data)
+            else:
+                return Response(
+                    {"user": "The provided user does not match the targeted account."}
+                )
+        else:
+            return Response(serializer.errors)
+
+
+class TransactionView(viewsets.ModelViewSet):
+    serializer_class = TransactionSerializer
+    queryset = Transaction.objects.all()
+    filter_backends = [OrderingFilter, DjangoFilterBackend]
+    filterset_fields = ("account",)
+    ordering_fields = ("date", "value", "payee")
