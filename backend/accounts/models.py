5cbc0c4898534b2728aa4f197e7986267a2ea315
Pierre Tessier <pierre.borie.tessier@gmail.com>
2024-02-16 19:17:26 -0800

    add transaction_hash, balance, support for ofx update

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,11 +1,11 @@
-from pathlib import Path
-
 from currencies.models import Currency
 from django.db import models
+from django.db.models import Sum
+from django.db.utils import IntegrityError
 from django.utils.translation import gettext_lazy as _
 from users.models import User
 
-from .utils import ofxutils
+from .utils import ofxutils, uuid
 
 
 class AccountType(models.IntegerChoices):
@@ -37,8 +37,12 @@ class Account(models.Model):
     def __str__(self) -> str:
         return "{} ({})".format(self.name, self.currency.identifier)
 
+    def get_balance(self):
+        transactions = self.transaction_set.all()
+        return transactions.aggregate(balance=Sum("value", default=0))["balance"]
+
     @classmethod
-    def create_from_file(cls, user: User, filename: str, **kwargs):
+    def create_from_file(cls, user: User, filetype: str, content: str, **kwargs):
         """_summary_
 
         Args:
@@ -52,35 +56,78 @@ class Account(models.Model):
         Returns:
             _type_: _description_
         """
-        filename = Path(filename)
-        if not filename.exists():
-            raise FileNotFoundError(
-                "Provided file does not exist: {}".format(str(filename))
-            )
-
-        if filename.suffix == ".ofx":
-            account, transactions = cls.from_ofx_file(user, filename, **kwargs)
+        if filetype == "ofx":
+            account, transactions = cls.from_ofx_file(content, **kwargs)
         else:
             raise NotImplementedError(
-                "File extension not supported: {}".format(filename.suffix)
+                "File extension not supported: {}".format(filetype)
             )
 
+        account.user = user
         account.save()
         for transaction in transactions:
-            transaction.save()
+            try:
+                transaction.save()
+            except IntegrityError:
+                continue
 
         return account
 
-    @classmethod
-    def from_ofx_file(cls, user: User, filename: Path, **kwarg):
+    def update_from_file(self, filetype: str, content: str, **kwargs):
+        """_summary_
+
+        Args:
+            user (User): _description_
+            filename (str): _description_
+
+        Raises:
+            FileNotFoundError: _description_
+            NotImplementedError: _description_
+
+        Returns:
+            _type_: _description_
+        """
+        if filetype == "ofx":
+            transactions = self.transactions_from_ofx_file(content, **kwargs)
+        else:
+            raise NotImplementedError(
+                "File extension not supported: {}".format(filetype)
+            )
+
+        written_transactions = []
+        for transaction in transactions:
+            try:
+                transaction.save()
+                written_transactions.append(transaction)
+            except IntegrityError:
+                continue
+
+        return written_transactions
+
+    def transactions_from_ofx_file(self, content: str, **kwarg):
         """_summary_
 
         Args:
             user (User): _description_
             filename (Path): _description_
         """
-        data = ofxutils.OfxData.from_file(filename)
-        account = cls.from_ofx_data(user, data.account, **kwarg)
+        data = ofxutils.OfxData.from_file(content)
+        transactions = [
+            Transaction.from_ofx_data(transaction, self)
+            for transaction in data.transactions
+        ]
+        return transactions
+
+    @classmethod
+    def from_ofx_file(cls, content: str, **kwarg):
+        """_summary_
+
+        Args:
+            user (User): _description_
+            filename (Path): _description_
+        """
+        data = ofxutils.OfxData.from_file(content)
+        account = cls.from_ofx_data(data.account, **kwarg)
         transactions = [
             Transaction.from_ofx_data(transaction, account)
             for transaction in data.transactions
@@ -90,7 +137,6 @@ class Account(models.Model):
     @classmethod
     def from_ofx_data(
         cls,
-        user: User,
         data: dict,
         currency: Currency = Currency(identifier="USD"),
         name: str = None,
@@ -107,7 +153,6 @@ class Account(models.Model):
             Account: _description_
         """
         account = cls(
-            user=user,
             currency=currency,
             name=name if name is not None else data["institution"],
             account_type=data["type"] + 1,
@@ -125,18 +170,24 @@ class Transaction(models.Model):
     #     "categories.Category",
     #     models.RESTRICT,
     # )
-    payee = models.CharField(max_length=256)
-    memo = models.CharField(max_length=256)
+    payee = models.CharField(max_length=128)
+    memo = models.CharField(max_length=128)
     value = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField()
-
-    TransactionType = models.IntegerChoices("TransactionType", "DEBIT CREDIT")
-    transaction_type = models.IntegerField(choices=TransactionType)
+    transaction_hash = models.UUIDField(editable=False, unique=True)
 
     def __str__(self) -> str:
         return "{} - {} ({} {})".format(
             self.date, self.payee, self.value, self.account.currency.symbol
         )
+
+    def get_hash(self):
+        content = str(self.account.id) + self.payee + str(self.value) + str(self.date)
+        return uuid.create_uuid_from_string(content)
+
+    def save(self, *args, **kwargs):
+        self.transaction_hash = self.get_hash()
+        super().save(*args, **kwargs)
 
     @classmethod
     def from_ofx_data(cls, data: dict, account: Account):
@@ -149,28 +200,20 @@ class Transaction(models.Model):
         Returns:
             Transaction: _description_
         """
-        transaction_type = data["type"]
         transaction_value = data["amount"]
 
-        if transaction_type == "debit":
-            transaction_type = 0
-            transaction_value = -transaction_value
-        elif transaction_type == "credit":
-            transaction_type = 1
-        else:
-            raise NotImplementedError(
-                "Transaction type not supported: {}.".format(transaction_type)
-            )
-
         if account.account_type == AccountType.CREDIT:
-            transaction_value = -transaction_value
+            if data["type"] == "credit":
+                transaction_value = -transaction_value
+        else:
+            if data["type"] == "debit":
+                transaction_value = -transaction_value
 
         transaction = cls(
             account=account,
             payee=data["payee"],
             memo=data["memo"],
-            date=data["user_date"],
+            date=data["user_date"].date(),
             value=transaction_value,
-            transaction_type=transaction_type,
         )
         return transaction
